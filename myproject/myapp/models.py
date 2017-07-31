74a7b427ed7a18fe3f201630e7e5ff774be5e297
Manishkumar21 <khiladimanu1@gmail.com>
2017-07-31 15:31:18 +0530

    Login Problem Solved

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models
+import uuid
 
 # Create your models here.
 
--- myproject/myproject/views.py
+++ myproject/myproject/views.py
@@ -1,23 +1,20 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from django.shortcuts import render
-
 # Create your views here.
 
 from django.shortcuts import render, redirect
 from datetime import datetime
-from myapp.forms import SignUpForm,LoginForm,PostForm,LikeForm,CommentForm
-from myapp.models import User,SessionToken,PostModel,LikeModel,CommentModel
+from myapp.forms import SignUpForm, LoginForm, PostForm, LikeForm, CommentForm
+from myapp.models import User, SessionToken, PostModel, LikeModel, CommentModel
 from datetime import timedelta
 from django.utils import timezone
 from myproject.settings import BASE_DIR
-from django.contrib.auth.hashers import make_password,check_password
+from django.contrib.auth.hashers import make_password, check_password
 from django.http import HttpResponseRedirect
 from django.contrib.auth import logout
 import smtplib
 from constants import constant
-
 
 from imgurpython import ImgurClient
 
@@ -26,90 +23,88 @@ client_sec = "296a3fb33f4cfff095f07a8f24f50e930361445c"
 
 
 # Create your views here.
-def signup_view(request) :
-    
-    #Business Logic starts here
+def signup_view(request):
+    # Business Logic starts here
 
-    if request.method=='GET' :  #IF GET REQUEST IS RECIEVED THEN DISPLAY THE SIGNUP FORM
-        #today=datetime.now()
+    if request.method == 'GET':  # IF GET REQUEST IS RECIEVED THEN DISPLAY THE SIGNUP FORM
+        # today=datetime.now()
         form = SignUpForm()
-        #template_name='signup.html'
-        return render(request,'signup.html',{'form':form})
+        # template_name='signup.html'
+        return render(request, 'signup.html', {'form': form})
 
-    elif request.method=='POST' :
+    elif request.method == 'POST':
         form = SignUpForm(request.POST)
-        if form.is_valid() : #Checks While Valid Entries Is Performed Or Not
-            username=form.cleaned_data['username']
-            email=form.cleaned_data['email']
-            name=form.cleaned_data['name']
-            password=form.cleaned_data['password']
-            #here above cleaned_data is used so that data could be extracted in safe manner,checks SQL injections
+        if form.is_valid():  # Checks While Valid Entries Is Performed Or Not
+            username = form.cleaned_data['username']
+            email = form.cleaned_data['email']
+            name = form.cleaned_data['name']
+            password = form.cleaned_data['password']
+            # here above cleaned_data is used so that data could be extracted in safe manner,checks SQL injections
 
-            #following code inserts data into database
+            # following code inserts data into database
 
-            new_user=User(name=name,password=make_password(password),username=username,email=email)
-            new_user.save()   #finally saves the data in database
+            new_user = User(name=name, password=make_password(password), username=username, email=email)
+            new_user.save()  # finally saves the data in database
 
-
-            #sending welcome Email To User That Have Signup Successfull	
-	    message="Welcome!! you Creating Your Account in swatchbharat clean india  by dushant kumar.You Have " \
+            # sending welcome Email To User That Have Signup Successfull
+            message = "Welcome!! you Creating Your Account in swatchbharat clean india  by dushant kumar.You Have " \
                       "Successfully Registered.It is correct place for show people about your work how you clean ur india.We Are Happy To Get You" \
                       "as one of our member "
-            server = smtplib.SMTP('smtp.gmail.com',587)
+            server = smtplib.SMTP('smtp.gmail.com', 587)
             server.starttls()
-            server.login('dushantk1@gmail.com',constant)
-            server.sendmail('dushantk1@gmail.com',email,message)
+            server.login('dushantk1@gmail.com', constant)
+            server.sendmail('dushantk1@gmail.com', email, message)
             #   WOW!!!SUCCESSFULLY SEND EMAIL TO THE USER WHO HAS SIGNUP.USER CAN CHECK INBOX OR SPAM
             # THIS IS ACCURATLY WORKING
-        return render(request,'success.html',{'form': form})
+        return render(request, 'success.html', {'form': form})
 
-#-------------------------------------create a new function for login  user---------------------------------------------------------
+
+# -------------------------------------create a new function for login  user---------------------------------------------------------
 def login_view(request):
-    #----------------------------------here is the function logic-----------------------------------------------------------------
+    # ----------------------------------here is the function logic-----------------------------------------------------------------
     if request.method == 'GET':
-        #Display Login Page
+        # Display Login Page
         login_form = LoginForm()
         template_name = 'login.html'
-    #---------------------------------------Elif part---------------------------------------------------------------------------------
+    # ---------------------------------------Elif part---------------------------------------------------------------------------------
     elif request.method == 'POST':
-        #Process The Data
+        # Process The Data
         login_form = LoginForm(request.POST)
         if login_form.is_valid():
-            #Validation Success
+            # Validation Success
             username = login_form.cleaned_data['username']
             password = login_form.cleaned_data['password']
-            #read Data From db
+            # read Data From db
             user = User.objects.filter(username=username).first()
             if user:
-                #compare Password
+                # compare Password
                 if check_password(password, user.password):
-                    token = SessionToken(user = user)
+                    token = SessionToken(user=user)
                     token.create_token()
                     token.save()
-                    response = redirect('feed/')
+                    response = redirect('/feed/')
                     response.set_cookie(key='session_token', value=token.session_token)
                     return response
-                    #successfully Login
+                    # successfully Login
 
                     template_name = 'login_success.html'
                 else:
 
-                    #Failed
+                    # Failed
                     template_name = 'login_fail.html'
             else:
-                #user doesn't exist
+                # user doesn't exist
                 template_name = 'login_fail.html'
         else:
-            #Validation Failed
+            # Validation Failed
             template_name = 'login_fail.html'
 
+    return render(request, template_name, {'login_form': login_form})
 
-    return render(request,template_name,{'login_form':login_form})
 
-#-------------------------------------------Create a new function for post --------------------------------------------------------------
+# -------------------------------------------Create a new function for post --------------------------------------------------------------
 def post_view(request):
-    
-    #-----------------------------------------here is the function logic------------------------------------------------------------
+    # -----------------------------------------here is the function logic------------------------------------------------------------
     user = check_validation(request)
 
     if user:
@@ -121,27 +116,28 @@ def post_view(request):
                 post = PostModel(user=user, image=image, caption=caption)
                 post.save()
 
-                path = str(BASE_DIR+"//"+post.image.url)
+                path = str(BASE_DIR + "//" + post.image.url)
 
-                client = ImgurClient(client_id,client_sec)
-                post.image_url = client.upload_from_path(path,anon=True)['link']
+                client = ImgurClient(client_id, client_sec)
+                post.image_url = client.upload_from_path(path, anon=True)['link']
                 post.save()
 
                 return redirect('/feed/')
 
         else:
             form = PostForm()
-        return render(request, 'post.html', {'form' : form})
+        return render(request, 'posts.html', {'form': form})
     else:
         return redirect('/login/')
 
-#--------------------------------------------Create a new functions to show the all post of user--------------------------------------
+
+# --------------------------------------------Create a new functions to show the all post of user--------------------------------------
 def feed_view(request):
     user = check_validation(request)
     if user:
-        #-------------------------------------here is the functions logic---------------------------------------------------------------
+        # -------------------------------------here is the functions logic---------------------------------------------------------------
 
-        posts = PostModel.objects.all().order_by('-created_on',)
+        posts = PostModel.objects.all().order_by('-created_on', )
 
         for post in posts:
 
@@ -149,17 +145,15 @@ def feed_view(request):
             if existing_like:
                 post.has_liked = True
 
-
-        return render(request, 'feed.html', {'posts': posts})
+        return render(request, 'feeds.html', {'posts': posts})
     else:
 
         return redirect('/login/')
 
 
-
-#----------------------------------------------Create a new functions to like the user post-------------------------------------------
+# ----------------------------------------------Create a new functions to like the user post-------------------------------------------
 def like_view(request):
-    #-------------------------------------------here is the function logic------------------------------------------------------------
+    # -------------------------------------------here is the function logic------------------------------------------------------------
     user = check_validation(request)
     if user and request.method == 'POST':
         form = LikeForm(request.POST)
@@ -176,9 +170,10 @@ def like_view(request):
     else:
         return redirect('/login/')
 
-#------------------------------------------------Create a new functions to comment on a user post---------------------------------------
+
+# ------------------------------------------------Create a new functions to comment on a user post---------------------------------------
 def comment_view(request):
-    #----------------------------------------------here is the function logic-------------------------------------------------------
+    # ----------------------------------------------here is the function logic-------------------------------------------------------
     user = check_validation(request)
     if user and request.method == 'POST':
         form = CommentForm(request.POST)
@@ -196,11 +191,9 @@ def comment_view(request):
         return redirect('/login')
 
 
-
-
 # -----------------------------------------------Create a functions for validating the session---------------------------------------------
 def check_validation(request):
-    #----------------------------------------------here is the function logic----------------------------------------------------------
+    # ----------------------------------------------here is the function logic----------------------------------------------------------
     if request.COOKIES.get('session_token'):
         session = SessionToken.objects.filter(session_token=request.COOKIES.get('session_token')).first()
         if session:
@@ -210,6 +203,7 @@ def check_validation(request):
     else:
         return None
 
+
 def logout_page(request):
-	logout(request)
+    logout(request)
 
